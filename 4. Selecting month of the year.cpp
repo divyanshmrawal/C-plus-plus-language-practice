4c804c544719fe3bd25847ed11f454824d77b1c7
DIVYANSHMRAWAL <divyanshmrawal@gmail.com>
2024-06-20 10:55:23 +0530

    Create 4. Selecting month of the year.cpp
    C-program to select month of the year
    using user-input number.

---
+++ 4. Selecting month of the year.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	//Accepting user-input
+	int userinput;
+	cout<<"Enter number of month : ";
+	cin>>userinput;
+	
+	//Selecting monts on the base of user-input
+	switch(userinput){
+		case 1:
+			cout<<"Name of month --> January";
+		break;	
+		
+		case 2:
+			cout<<"Name of month --> February";
+		break;
+		
+		case 3:
+			cout<<"Name of month --> March";
+		break;
+		
+		case 4:
+			cout<<"Name of month --> April";
+		break;
+		
+		case 5:
+			cout<<"Name of month --> May";
+		break;
+		
+		case 6:
+			cout<<"Name of month --> June";
+		break;
+		
+		case 7:
+			cout<<"Name of month --> July";
+		break;
+		
+		case 8:
+			cout<<"Name of month --> August";
+		break;
+		
+		case 9:
+			cout<<"Name of month --> September";
+		break;
+		
+		case 10:
+			cout<<"Name of month --> October";
+		break;
+		
+		case 11:
+			cout<<"Name of month --> November";
+		break;
+		
+		case 12:
+			cout<<"Name of month --> December";
+		break;
+	}
+	
+	return 0;
+}
