b8c48eca47cf25c007cee755e8846026d13eb3b1
Divyansh Rawal <divyanshmrawal@gmail.com>
2024-06-20 12:37:42 +0530

    Update and rename 6.Counting of different numbers.cpp to 6.Counting of different type of numbers.cpp
    Update and rename 6.Counting of different numbers.cpp to 6.Counting of different type of numbers.cpp

--- 6.Counting of different type of numbers.cpp
+++ 6.Counting of different type of numbers.cpp
@@ -1,28 +1,38 @@
+//QUESTION --> Write C program that reads 5 numbers and counts the number of positive numbers and negative numbers.
+
+//ANSWER -->
 #include <iostream>
 using namespace std;
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
+	//Variable Declaration
 	int arr[100] , i , size, positive=0 , negative=0;
-	
+
+	//Accepting size of array 
 	cout<<"Enter size of the array : ";
 	cin>>size;
-	
+
+	//loop-1
 	for(i=0;i<size;i++){
+		//Accepting numbers from user 
 		cout<<"Enter your numbers : ";
 		cin>>arr[i];
 	
-	
+	//If first condition is true 
 	if(arr[i]>0){
+		//then count positive numbers
 		positive=positive+1;
 	}
-	
+
+	//If secound condition is true 	
 	if(arr[i]<0){
+		//Then count negative numbers
 		negative=negative+1;
 	}
 }
+	//Print Numbers of positive values
 	cout<<"Numbers of positive values are : "<<positive <<"\n";
-	
+	//Print Numbers of negative values
 	cout<<"Numbers of negative values are : "<<negative;
 
 	return 0;
