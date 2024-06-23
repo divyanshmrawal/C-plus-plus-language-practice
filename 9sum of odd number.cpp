c4b414d455dfd1f0a5077266d5a9e3cc5e620cb5
DIVYANSHMRAWAL <divyanshmrawal@gmail.com>
2024-06-23 10:01:11 +0530

    Create 9sum of odd number.cpp
    sum of odd number

---
+++ 9sum of odd number.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+int i,arr[100],start,end,sum=0 , list;
+
+cout<<"Enter your starting number : ";
+cin>>start;
+
+cout<<"Enter your ending number : ";
+cin>>end;
+
+
+for(i=start;i<=end;i++){
+		
+	cout<<"Enter your number : ";
+	cin>>arr[i];
+	
+   if((arr[i]%2) != 0){
+   
+   sum=sum+arr[i];
+   	
+}
+
+}
+
+cout<<"sum of all odd number : "<<sum<<"\n";
+
+	return 0;
+}
