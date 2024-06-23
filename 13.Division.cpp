d96474d156bd7ed643e8dcdb36cb403f38d4bc22
Divyansh Rawal <divyanshmrawal@gmail.com>
2024-06-23 16:51:16 +0530

    Update 13.Division.cpp
    Update 13.Division.cpp

--- 13.Division.cpp
+++ 13.Division.cpp
@@ -1,3 +1,7 @@
+//QUESTION -->
+//Write a program that reads two numbers and divides the first number by the second number. If division is not possible print "Division is not possible".
+
+//ANSWER-->
 #include <iostream>
 
 using namespace std;
