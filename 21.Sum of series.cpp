7826ec65d6bc83c637373a37c767d7c63a14fb03
DIVYANSHMRAWAL <divyanshmrawal@gmail.com>
2024-06-24 11:18:01 +0530

    Create 21.Sum of series.cpp
    C program that finds all the divisors of an integer.

---
+++ 21.Sum of series.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int userinput , i;
+	//get an user input
+	cout<<"Enter your number : ";
+	cin>>userinput;
+	        
+    //loop-1
+    cout<<"All divisors of userinput :"<<userinput<<"\n";
+	for(i=1;i<=userinput;i++){
+		//If condition is true then
+		if((userinput%i)==0){
+			//print it--
+			cout<<""<<i<<"\n";
+		}
+		
+	}
+	return 0;
+}
