ce1426b16ac43ad90791062339e2bd63930b9744
Divyansh Rawal <divyanshmrawal@gmail.com>
2024-06-25 11:36:18 +0530

    30. Maximum and minimum values of array.cpp
    30. Maximum and minimum values of array.cpp

--- 30. Maximum and minimum values of array.cpp
+++ 30. Maximum and minimum values of array.cpp
@@ -1,18 +1,22 @@
+//QUESTION-->
+//Write a C-program to find max and min number in array
+
+//ANSWER-->
 #include <iostream>
 
 using namespace std;
 
 int main() {
 	int i,size,arr[100],sum,x,y;
-	
+	//Accepting total numbers of values
 	cout<<"Enter total number of values : ";
 	cin>>size;
-	
+	//loop-1
 	for(i=0;i<size;i++){
-		
+		//Accepting number from user
 		cout<<"Enter your number : ";
 		cin>>arr[i];
-		
+		//Printing smallest number in array
 		if(arr[0]>arr[i]){
 			arr[0]=arr[i];
 			x=arr[i];
@@ -21,7 +25,7 @@ int main() {
 	}
 		cout<<"Smallest number :"<<arr[0]<<"\n";
 		
-		
+		//Printing largest number in array
 		for(i=0;i<size;i++){
 		if(arr[0]<arr[i]){
 			arr[0]=arr[i];
