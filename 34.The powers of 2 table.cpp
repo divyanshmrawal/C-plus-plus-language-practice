ee756b0271ccc344c216c1acfdfdae47514b924a
DIVYANSHMRAWAL <divyanshmrawal@gmail.com>
2024-06-26 09:47:51 +0530

    Create 34.The powers of 2 table.cpp
     C program that prints the powers of 2 table for the powers 0 to 10, both positive and negative.

---
+++ 34.The powers of 2 table.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+
+int main() {
+	int i;
+	for(i=0;i<=10;i++){
+		cout<<i<<"\t"<<"Positive pawer of 2 -->"<<pow(2,i)<<"\t"<<"Negative power of 2 -->"<<pow(2,-i)<<"\t"<<"\n";
+}	
+	return 0;
+}
