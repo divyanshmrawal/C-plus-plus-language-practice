b60fd502f5022b717b3a0d49e116e2ec02203098
Divyansh Rawal <divyanshmrawal@gmail.com>
2024-06-26 10:33:02 +0530

    35. Series question
    35. Series question

--- 35. Series question.cpp
+++ 35. Series question.cpp
@@ -1,19 +1,26 @@
+//QUESTION-->
+//Write a C program to display the sum of series 1 + 1/2 + 1/3 + ………. + 1/n.
+
+//ANSWER-->
 #include <iostream>
 
 using namespace std;
 
 int main() {
 	//x=userinput last number
-	
 	float i,x,sum=0;
 	
+	//Accepting last number from user 
 	cout<<"Enter last number : ";
 	cin>>x;
-	
+
+	//loop-1
 	for(i=1;i<=x;i++){
+		//Sum of series
 		sum=sum+(1/i);
 	}
-	
+
+	//printing result
 	cout<<"sum of series : "<<sum;
 	return 0;
 }
