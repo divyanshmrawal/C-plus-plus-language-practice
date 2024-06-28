7010b8ea1f553fcc07c737088b564ff1802db282
DIVYANSHMRAWAL <divyanshmrawal@gmail.com>
2024-06-28 11:00:05 +0530

    Create 46.Printing size of C's datatypes.cpp
     C program to display the sizes and ranges for each of C's data types

---
+++ 46.Printing size of C's datatypes.cpp
@@ -0,0 +1,28 @@
+//QUESTION-->
+//Write a C program to display the sizes and ranges for each of C's data types
+
+//ANSWER-->
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	//Variable declaration
+	char x;
+	int i;
+	double d;
+	float f;
+	
+	//printing heading
+	cout<<"NAME  : "<<"\t"<<"SIZE"<<"\n";
+	cout<<"---------------------"<<"\n";
+	
+	//printing name and size of variables
+	cout<<"char  : "<<"\t"<<sizeof(x)<<"\n";
+	cout<<"int   : "<<"\t"<<sizeof(i)<<"\n";
+	cout<<"double: "<<"\t"<<sizeof(d)<<"\n";
+	cout<<"float : "<<"\t"<<sizeof(f)<<"\n";
+	
+	
+	return 0;
+}
