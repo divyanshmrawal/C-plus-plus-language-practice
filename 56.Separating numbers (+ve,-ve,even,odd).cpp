d077842b2c4363637a291284d278450d3d807f3a
Divyansh Rawal <divyanshmrawal@gmail.com>
2024-06-29 16:11:39 +0530

    56.Separating numbers (+ve,-ve,even,odd)
     56.Separating numbers (+ve,-ve,even,odd)

--- 56.Separating numbers (+ve,-ve,even,odd).cpp
+++ 56.Separating numbers (+ve,-ve,even,odd).cpp
@@ -1,16 +1,22 @@
+//QUESTION-->
+//Write a C program that accepts 7 integer values and counts the even, odd, positive and negative values
+
+//ANSWER-->
 #include <iostream>
 
 using namespace std;
 
 int main() {
+	//Variables declaration 
 int x,positive=0,negative=0,even=0,odd=0;
 	int i;
 	
+	//Accepting number from user
 	for(i=0;i<7;i++){
 		cout<<"Enter your "<<i<<" number : ";
 		cin>>x;
 	
-	
+	//Calculating and printing total number of positive numbers , negative numbers , even numbers , odd numbers
 	if(x>0){
 		positive=positive+1;
 	}
