35d59ffee0326dcbeb36831c1c3c4bf3083e8788
DIVYANSHMRAWAL <divyanshmrawal@gmail.com>
2024-06-30 13:15:11 +0530

    Create 70.Inserting a comma between numbers.cpp
    C program to print a sequence from 1 to a given (integer) number, inserting a comma between these numbers. There will be no comma after the last character

---
+++ 70.Inserting a comma between numbers.cpp
@@ -0,0 +1,15 @@
+//QUESTION-->
+//Write a C program to print a sequence from 1 to a given (integer) number, inserting a comma between these numbers. There will be no comma after the last character
+
+//ANSWER-->
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int i;
+	for(i=1;i<=10;i++){
+		cout<<","<<i;
+	}
+	return 0;
+}
