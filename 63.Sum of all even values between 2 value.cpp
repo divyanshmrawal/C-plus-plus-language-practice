b4a8a7d9430993f062b297e643432264e3bde8c0
Divyansh Rawal <divyanshmrawal@gmail.com>
2024-06-30 10:06:38 +0530

    63.Sum of all even values between 2 value
    63.Sum of all even values between 2 value

--- 63.Sum of all even values between 2 value.cpp
+++ 63.Sum of all even values between 2 value.cpp
@@ -1,16 +1,23 @@
+//QUESTION-->
+//Write a C program that accepts two integer values and calculates the sum of all even values between them
+
+//ANSWER-->
 #include <iostream>
 
 using namespace std;
 
 int main() {
+	//Variable declaration 
 	int st,en,i,sum=0;
-	
+
+	//Accepting numbers from user 
 	cout<<"Enter starting number : ";
 	cin>>st;
 	
 	cout<<"Enter last number : ";
 	cin>>en;
-	
+
+	//Sum of all even numbers between 2 numbers
 	if(st<en){
 		
 	for(i=st;i<en;i++){
@@ -18,6 +25,7 @@ int main() {
 			sum=sum+i;
 		}
 	}
+	//printing result
 	cout<<"Sum of even number is : "<<sum;
 }
 
